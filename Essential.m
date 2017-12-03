fd17e1e86d7c471bc03448ce475c7ace6caceff9
chong-gnubio <minsung.hong0320@gmail.com>
2017-12-02 23:47:26 -0500

    Update Essential.m

--- Essential.m
+++ Essential.m
@@ -1,10 +1,8 @@
-function [E,R,T] = Essential(img1,img2, CameraParams)
+function [E,R,T] = Essential(img1,img2)
 %ESSENTIAL Summary of this function goes here
 %   This function calculates the relative position and orientation
 %   (essential matrix) of camera2 wrt camera1
-%   Input: img1, img2, images taken by from two view points
-%           CameraParams are intrisinc parameters of the camera, such as
-%           focal point and priciple point
+%   Input: img1, img2, images are grayscale images taken by from two view points
 
 %   Output: The rotation matrix in form of [r11 r12 r13
 %                                           r21 r22 r23
@@ -20,28 +18,28 @@ load upToScaleReconstructionCameraParameters.mat
 % imageDir = '/Users/mshong0320/Desktop/CV_Project_Data/From_spot3';
 % images = imageDatastore(imageDir);
 
-img1 = undistortImage(readimage(images,1),cameraParams);
-img2 = undistortImage(readimage(images,2),cameraParams);
-
-img1_gray= rgb2gray(I1);
-img2_gray = rgb2gray(I2);
+% img1 = undistortImage(readimage(images,1),cameraParams);
+% img2 = undistortImage(readimage(images,2),cameraParams);
+% 
+% img1_gray= rgb2gray(I1);
+% img2_gray = rgb2gray(I2);
 
 %%
 
 %Detect feature points each image.
-imagePoints1 = detectSURFFeatures(img1_gray); %function derives the descriptors from pixels surrounding an interest point.
-imagePoints2 = detectSURFFeatures(img2_gray);
+imagePoints1 = detectSURFFeatures(img1); %function derives the descriptors from pixels surrounding an interest point.
+imagePoints2 = detectSURFFeatures(img2);
 
-corners1 = detectHarrisFeatures(img1_gray); 
-corners2 = detectHarrisFeatures(img2_gray);
+corners1 = detectHarrisFeatures(img1); 
+corners2 = detectHarrisFeatures(img2);
 
-[features1, valid_corners1] = extractFeatures(img1_gray, corners1);
-[features2, valid_corners2] = extractFeatures(img2_gray, corners2);
-[SURFfeatures1, imagePoints1] = extractFeatures(img1_gray, imagePoints1);
-[SURFfeatures2, imagePoints2] = extractFeatures(img2_gray, imagePoints2);
+[features1, valid_corners1] = extractFeatures(img1, corners1);
+[features2, valid_corners2] = extractFeatures(img2, corners2);
+[SURFfeatures1, imagePoints1] = extractFeatures(img1, imagePoints1);
+[SURFfeatures2, imagePoints2] = extractFeatures(img2, imagePoints2);
 
 figure; 
-imshow(img1_gray);
+imshow(img1);
 hold on
 plot(valid_corners1);
 plot(imagePoints1.selectStrongest(10),'showOrientation',true);
@@ -50,8 +48,8 @@ hold off
 %%
 
 % Extract feature descriptors from each image.
-SURFfeatures1 = extractFeatures(img1_gray,imagePoints1,'Upright',true);
-SURFfeatures2 = extractFeatures(img2_gray,imagePoints2,'Upright',true);
+SURFfeatures1 = extractFeatures(img1,imagePoints1,'Upright',true);
+SURFfeatures2 = extractFeatures(img2,imagePoints2,'Upright',true);
 
 
 %%
@@ -62,7 +60,7 @@ matchedPoints1 = imagePoints1(indexPairs(:,1));
 matchedPoints2 = imagePoints2(indexPairs(:,2));
 
 figure
-showMatchedFeatures(img1_gray,img2_gray, matchedPoints1,matchedPoints2, 'montage', 'PlotOptions',{'ro','go','y--'});
+showMatchedFeatures(img1,img2, matchedPoints1,matchedPoints2, 'montage', 'PlotOptions',{'ro','go','y--'});
 
 title('Putative Matches')
 %%
@@ -75,7 +73,7 @@ E
 inlierPoints1 = matchedPoints1(inliers);
 inlierPoints2 = matchedPoints2(inliers);
 figure
-showMatchedFeatures(img1_gray,img2_gray,inlierPoints1,inlierPoints2, 'montage', 'PlotOptions',{'ro','go','y--'});
+showMatchedFeatures(img1,img2,inlierPoints1,inlierPoints2, 'montage', 'PlotOptions',{'ro','go','y--'});
 title('Inlier Matches with outer points removed')
 
 % R = eye(3);
